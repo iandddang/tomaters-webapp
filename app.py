e8d187df793f1a21d8d6b3356291fb0e531fdd4a
Ian Dang <iandang@dhcp-v053-006.mobile.uci.edu>
2018-11-20 11:54:56 -0800

    Added flask socketio.

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 from flask import Flask, request, jsonify, render_template
+from flask_socketio import SocketIO
+from time import sleep
+import random
+import threading
 
 # flask config
 app = Flask(__name__)
+
+# websocket object
+socket = SocketIO(app)
+
+# test thread function
+def generate_random_number():
+    while True:
+        random_float = round(random.uniform(0, 100), 2)
+        socket.emit('new_signal', random_float)
+        sleep(2.5)
 
 @app.route('/', methods=['GET'])
 def home():
     return render_template('home.html', page='home')
 
 if __name__ == "__main__":
+    socket_test_thread = threading.Thread(target=generate_random_number)
+    socket_test_thread.start()
+
     app.run(host='0.0.0.0', port=8080, debug=True, use_reloader=True)
