ab66eb5bea1080449e0600e8eb74493c82ba2e4c
Ian Dang <iandddang@gmail.com>
2019-02-17 18:41:21 -0800

    Added login page.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, session
 from flask_socketio import SocketIO
 from PlotManager import PlotManager
 from PlantManager import PlantManager
@@ -10,6 +10,7 @@ import plotly
 
 # flask config
 app = Flask(__name__)
+app.secret_key = "top secret key"
 
 # websocket object
 socket = SocketIO(app)
@@ -27,20 +28,35 @@ def generate_random_number():
 
 @app.route('/', methods=['GET'])
 def home():
+        if session.get('logged_in'):
+                graphs = plot_manager.graphs
 
-        graphs = plot_manager.graphs
+                # Add "ids" to each of the graphs to pass up to the client
+                # for templating
+                ids = ['graph-{}'.format(i) for i, _ in enumerate(graphs)]
 
-        # Add "ids" to each of the graphs to pass up to the client
-        # for templating
-        ids = ['graph-{}'.format(i) for i, _ in enumerate(graphs)]
+                # Convert the figures to JSON
+                # PlotlyJSONEncoder appropriately converts pandas, datetime, etc
+                # objects to their JSON equivalents
+                graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
 
-        # Convert the figures to JSON
-        # PlotlyJSONEncoder appropriately converts pandas, datetime, etc
-        # objects to their JSON equivalents
-        graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+                return render_template('home.html', page='home', cls=plotly.utils.PlotlyJSONEncoder, ids=ids, graphJSON=graphJSON, plant_manager=plant_manager)
+        else:
+                return render_template('login.html')
 
-        return render_template('home.html', page='home', cls=plotly.utils.PlotlyJSONEncoder, ids=ids, graphJSON=graphJSON, plant_manager=plant_manager)
 
+@app.route('/login', methods=['POST'])
+def login():
+        try:
+                status = "Login failed, incorrect authentication!"
+                if request.json['password'] == 'EECS159A' and request.json['username'] == 'Tomater':
+                        session['logged_in'] = True
+                        status = "Login success!"
+        except Exception as e:
+                status = 'Error caught: ' + str(e)
+        
+        return status
+        
 
 @app.route('/lighttoggle', methods=['GET', 'POST'])
 def toggle_light():
